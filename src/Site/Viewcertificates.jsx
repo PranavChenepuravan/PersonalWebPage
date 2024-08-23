1644e09a16548fa16a17715cdb5cbc96fafc1ddb
Pranav <pranavparathakkad@gmail.com>
2024-08-23 20:50:56 +0530

    Certificates added3

--- src/Site/Viewcertificates.jsx
+++ src/Site/Viewcertificates.jsx
@@ -8,7 +8,7 @@ export const Viewcertificates = () => {
     
 <a href="#" class="mt-2 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Download Certificates</h5>
     <div >
       <Certificates />
     </div>
