217c8ce3cebb5101b5039a13c5f5d69dd9e3581d
Pranav <pranavparathakkad@gmail.com>
2024-08-30 11:16:28 +0530

    All certificates

--- src/Site/Certificates.jsx
+++ src/Site/Certificates.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const certificates = [
   { name: "Resume", file: "PRANAV C_MERN DEVELOPER2-Latest.pdf" },
-  { name: "Provisional Certificate", file: "AWH22MCA-2030_TueJul23_Provisional Certificate_179661.pdf" },
-  { name: "Grade Card", file: "AWH22MCA-2030_TueJul23_Consolidated Grade Card_283236.pdf" },
+  { name: "MCA Provisional Certificate", file: "AWH22MCA-2030_TueJul23_Provisional Certificate_179661.pdf" },
+  { name: "MCA Grade Card", file: "AWH22MCA-2030_TueJul23_Consolidated Grade Card_283236.pdf" },
+  { name: "PGDC Certificate", file: "LBSCertificate.pdf" },
+  { name: "PGDC Grade Card", file: "PGDCA_Gradecard.pdf" },
+  { name: "B.A Engilsh Certificate", file: "BACertificate.pdf" },
+  { name: "B.A Engilsh Grade Card", file: "BAGradecard.pdf" },
+  { name: "Plus Two", file: "PlusTwo.pdf" },
+  { name: "SSLC", file: "SSLC.pdf" },
   // Add more certificates as needed
 ];
 
